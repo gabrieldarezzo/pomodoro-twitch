2bba0f339719e4e43e4fc9188a9efb00542fbc69
Gabriel Darezzo <Jussi01@J01243.jussi.local>
2020-03-12 10:17:01 -0300

    Initial dist subtree commit

---
+++ precache-manifest.3e1b7d652b4543e9df1806751abf55f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8d2d52ae3674af792d72",
+    "url": "/js/chunk-vendors.3a422538.js"
+  },
+  {
+    "revision": "05a542246f16f4a54955",
+    "url": "/js/app.7958fa25.js"
+  },
+  {
+    "revision": "1083112a33e59036d0ff",
+    "url": "/js/about.2dd250df.js"
+  },
+  {
+    "revision": "5da9bdb35f1ec8dbc973db718fa9ffea",
+    "url": "/index.html"
+  }
+];
